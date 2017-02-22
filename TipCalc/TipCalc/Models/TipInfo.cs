d20c8b1d372b01d31b1705e938f97278bf3ef6a9
josedl4 <joseop21@gmail.com>
2017-02-22 13:16:27 +0100

    Initial commit

---
+++ TipCalc/TipCalc/Models/TipInfo.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipCalc.Models
+{
+    class TipInfo
+    {
+        private decimal _total;
+        private decimal _subTotal;
+        private decimal _tipPercent;
+
+        public decimal Total
+        {
+            get { return _total; }
+            set
+            {
+                if(_total != value)
+                {
+                    _total = value;
+                    OnTipValueChanged();
+                }
+            }
+        }
+
+        public decimal Subtotal
+        {
+            get { return _subTotal; }
+            set
+            {
+                if (_subTotal != value)
+                {
+                    _subTotal = value;
+                    OnTipValueChanged();
+                }
+            }
+        }
+
+        public decimal TipPercent
+        {
+            get { return _tipPercent; }
+            set
+            {
+                if (_tipPercent != value)
+                {
+                    _tipPercent = value;
+                    OnTipValueChanged();
+                }
+            }
+        }
+
+        private void OnTipValueChanged()
+        {
+            var h = TipValueChanged;
+            if (h != null)
+                h(this, EventArgs.Empty);
+        }
+
+        public decimal Tax
+        {
+            get { return Total - Subtotal; }
+        }
+
+        public decimal TipValue
+        {
+            get
+            {
+                if (Total == 0m || Subtotal == 0m || TipPercent == 0m)
+                    return 0m;
+
+                var percent = TipPercent;
+                percent /= 100;
+                decimal value = (Subtotal * (1 + percent)) + (Total - Subtotal);
+                decimal fract = value - Math.Truncate(value);
+                int f = (int)(fract * 100);
+
+                while ((f % 25) != 0)
+                    ++f;
+
+                fract = f;
+                fract /= 100;
+                value = Math.Truncate(value) + fract;
+
+                return value - Total;
+            }
+        }
+
+        public event EventHandler TipValueChanged;
+    }
+}
---
+++ TipCalc/TipCalc/Views/MainView.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Xamarin.Forms;
+using TipCalc.Models;
+using System;
+
+namespace TipCalc.Views
+{
+    public partial class MainView : ContentPage
+    {
+        private TipInfo _info;
+
+        public MainView()
+        {
+            InitializeComponent();
+
+            _info = new TipInfo
+            {
+                TipPercent = 15
+            };
+
+            TipPercent.Text = _info.TipPercent.ToString();
+            TipPercentSlider.Value = Convert.ToDouble(_info.TipPercent);
+
+            _info.TipValueChanged += (sender, e) =>
+            {
+                TipValue.Text = _info.TipValue.ToString();
+                Total.Text = (_info.TipValue + _info.Total).ToString();
+            };
+
+            STotalPT.TextChanged += (sender, e) =>
+            {
+                _info.Total = Parse(STotalPT.Text);
+            };
+
+            TipPercent.TextChanged += (sender, e) =>
+            {
+                _info.TipPercent = Parse(TipPercent.Text);
+            };
+
+            STotal.TextChanged += (sender, e) =>
+            {
+                _info.Subtotal = Parse(STotal.Text);
+            };
+
+            TipPercentSlider.ValueChanged += (sender, e) =>
+            {
+                TipPercent.Text = e.NewValue.ToString();
+                TipPercentSlider.Value = e.NewValue;
+            };
+        }
+
+        static decimal Parse(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return 0m;
+
+            try
+            {
+                return Convert.ToDecimal(text);
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+                return 0m;
+            }
+        }
+    }
+}
